0ae7d3ef8985f91f70a322de5558f7b509c3c9e0
Everton Dias <evertonsrdias@gmail.com>
2025-10-17 20:32:13 -0300

    Mostrando palavra caso jogador tenha perdido

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() {
     let normalization_map = word::initialize_normalization_hashmap();
 
     let mut errors: i8 = 0;
+    let mut all_corrects = 0;
     let word = word::get_word();
     let mut visual_word = visual_effects::get_visual_word(&word);
     let mut all_guess: [char; LETTERS_IN_ALPHABET] = ['_'; LETTERS_IN_ALPHABET];
@@ -20,29 +21,40 @@ fn main() {
     loop {
         visual_effects::clear_screen();
         visual_effects::print_strength(errors);
-        visual_effects::print_word(&visual_word);
+        visual_effects::print_word(&visual_word); // TODO melhorar nome dessa função
         visual_effects::print_used_words(&all_guess);
 
         let guess = player_input::guess_letter(&mut input, &mut all_guess, &normalization_map);
-        if !check_is_correct(&mut visual_word, guess, &normalization_map) {
+        let corrects = check_corrects(&mut visual_word, guess, &normalization_map);
+
+        // TODO encapsular isso no end_game
+        if corrects == 0 {
             errors = errors + 1;
+        } else {
+            all_corrects = all_corrects + corrects;
+        }
+
+        if errors == 6 {
+            visual_effects::clear_screen();
+            print!("Você perdeu! :(\nA palavra era: {}\n", word);
+            break;
         }
     }
 }
 
-fn check_is_correct(
+fn check_corrects(
     word: &mut VisualWord,
     guess: char,
     normalization_map: &HashMap<char, char>,
-) -> bool {
-    let mut is_correct = false;
+) -> i32 {
+    let mut corrects = 0;
 
     for i in 0..word.size {
         if word::normalize_char(word.letters[i].c, &normalization_map) == guess {
             word.letters[i].visible = true;
-            is_correct = true;
+            corrects = corrects + 1;
         }
     }
 
-    return is_correct;
+    return corrects;
 }
